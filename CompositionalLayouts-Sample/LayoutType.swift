f1403595416e6148010ce380442c8c8403ca4410
Dai Kawahara <skbsporta7@gmail.com>
2020-01-06 23:32:50 +0900

    add LayoutType

---
+++ CompositionalLayouts-Sample/LayoutType.swift
@@ -0,0 +1,13 @@
+//
+//  LayoutType.swift
+//  CompositionalLayouts-Sample
+//
+//  Created by kawaharadai on 2020/01/06.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+enum LayoutType: CaseIterable {
+    case insta
+}
