4c3b2488f0510f1ca496382410d63a7d5cd7f0c9
Dai Kawahara <skbsporta7@gmail.com>
2020-01-13 18:58:57 +0900

    add layout like insta photo list

--- CompositionalLayouts-Sample/LayoutType.swift
+++ CompositionalLayouts-Sample/LayoutType.swift
@@ -24,13 +24,61 @@ enum LayoutType {
                                                            subitem: item,
                                                            count: 3)
             items.interItemSpacing = .fixed(itemSpacing)
-            items.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
             let groups = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                                                              heightDimension: itemLength),
                                                           subitems: [items])
             let sections = NSCollectionLayoutSection(group: groups) // ここでセルの数に反映
             sections.interGroupSpacing = itemSpacing
             sections.contentInsets = .init(top: 0, leading: itemSpacing, bottom: 0, trailing: itemSpacing)
+            let layout = UICollectionViewCompositionalLayout(section: sections)
+            return layout
+        case .insta:
+            // セル間のスペース
+            let itemSpacing = CGFloat(2)
+
+            // 小ブロック縦2グループ
+            let itemLength = (collectionViewBounds.width - (itemSpacing * 2)) / 3
+            let largeItemLength = itemLength * 2 + itemSpacing
+
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(itemLength),
+                                                                                 heightDimension: .absolute(itemLength)))
+            let verticalItemTwo = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(itemLength),
+                                                                                                      heightDimension: .absolute(largeItemLength)),
+                                                                   subitem: item,
+                                                                   count: 2)
+            verticalItemTwo.interItemSpacing = .fixed(itemSpacing)
+
+            // 大ブロックありグループ
+            let largeItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(largeItemLength),
+                                                                                      heightDimension: .absolute(largeItemLength)))
+
+            let largeItemLeftGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                                                           heightDimension: .absolute(largeItemLength)),
+                                                                        subitems: [largeItem, verticalItemTwo])
+            largeItemLeftGroup.interItemSpacing = .fixed(itemSpacing)
+
+            let largeItemRightGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                                                            heightDimension: .absolute(largeItemLength)),
+                                                                         subitems: [verticalItemTwo, largeItem])
+            largeItemRightGroup.interItemSpacing = .fixed(itemSpacing)
+
+            // 小ブロック縦2横3グループ
+            let twoThreeItemGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                                                          heightDimension: .absolute(largeItemLength)),
+                                                                       subitem: verticalItemTwo,
+                                                                       count: 3)
+            twoThreeItemGroup.interItemSpacing = .fixed(itemSpacing)
+
+            let subitems = [largeItemLeftGroup, twoThreeItemGroup, largeItemRightGroup, twoThreeItemGroup]
+            let groupsSpaceCount = CGFloat(subitems.count - 1)
+            let heightDimension = NSCollectionLayoutDimension.absolute(largeItemLength * CGFloat(subitems.count) + (itemSpacing * groupsSpaceCount))
+            // MEMO: 高さの計算は後に追加するスペース分も足す
+            let groups = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                                             heightDimension: heightDimension),
+                                                          subitems: subitems)
+            groups.interItemSpacing = .fixed(itemSpacing)
+            let sections = NSCollectionLayoutSection(group: groups)
+            sections.interGroupSpacing = itemSpacing
             let layout = UICollectionViewCompositionalLayout(section: sections)
             return layout
         default: return UICollectionViewLayout()
